46997a45ea83fa996680855a343429acee9b3072
Shaheen Ahmed <s.ahmed@aol.com>
2016-12-26 12:46:22 -0500

    ignores all .js and .map files

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Welcome to Angular 2!</h1>'
+    template: '<h1> Welcome to Angular 2!</h1>'
 })
 export class AppComponent { }
